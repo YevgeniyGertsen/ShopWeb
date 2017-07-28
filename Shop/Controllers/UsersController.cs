9a6c95cd2e2b34a28d4f3936ee19c9890e5833cc
y.gertsen <y.gertsen@ALA01-WEB01-TS>
2017-07-28 21:10:53 +0600

    Add project files.

---
+++ Shop/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using Users;
+
+namespace Shop.Controllers
+{
+    public class UsersController : Controller
+    {
+        Entity db = new Entity();
+        // GET: Users
+        public ActionResult Index()
+        {
+           List<User> users = db.Users.ToList();
+           // ViewBag.VisitHistory = db.VisitHistory.ToList();
+
+            return View(users);
+        }
+
+        public ActionResult Adduser(string Name)
+        {
+            User user = new User();
+            user.Name = Name;
+            db.Users.Add(user);
+
+            return RedirectToAction("Index");
+        }
+        //
+    }
+}
